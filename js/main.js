1fc1b0cca6d6216872f8eac10b960fa4663fd95e
Mona Zaghloul <95508036+monagadalrb@users.noreply.github.com>
2023-10-01 11:13:48 +0300

    New Project

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let feature = document.querySelector(".dropped-f");
+let company = document.querySelector(".dropped-c");
+let f_dropmenu = document.querySelector(".f .dropmenu-content");
+let c_dropmenu = document.querySelector(".c .dropmenu-content");
+
+function myFunction() {}
+window.onclick = function (event) {
+  if (event.target.matches(".dropped-c")) {
+    document.querySelector(".c .dropmenu-content").classList.toggle("show");
+    f_dropmenu.classList.remove("show");
+  } else if (event.target.matches(".dropped-f")) {
+    document.querySelector(".f .dropmenu-content").classList.toggle("show");
+    c_dropmenu.classList.remove("show");
+  } else {
+    f_dropmenu.classList.remove("show");
+    c_dropmenu.classList.remove("show");
+  }
+};
